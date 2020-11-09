1ff28f65aab4e7d7176f0a44d469d81729eb055d
KUMAR MANOJ MK <210333@mahindra.com>
2020-11-09 13:46:13 +0530

    add trajectory

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gamestate="onsling"
+
 
 
 function preload() {
@@ -68,17 +70,21 @@ function draw(){
     slingshot.display();    
 }
 
+
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gamestate!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate="launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        //slingshot.attach(bird.body);
     }
 }
